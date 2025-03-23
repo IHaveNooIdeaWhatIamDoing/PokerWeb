49bd86965150eb31594cc5cd02f45f7ed6585cb6
IHaveNooIdeaWhatIamDoing <domenik08sabitzer@gmail.com>
2025-03-23 17:52:12 +0100

    Start
    Karten, mehere Spieler, kleienr Code zum Karten spawnen

---
+++ main.js
@@ -0,0 +1,57 @@
+const symbols = ['clubs', 'diamonds', 'hearts', 'spades'];
+const numbers = ['2', '3', '4', '5', '6', '7', '8', '9', '10'];
+const HighCards = ['jack', 'queen', 'king', 'ace'];
+
+let cardPictures = [];
+let cardValues = [];
+
+//DOM elements
+const doc = document;
+const PlayerCardDiv = doc.querySelectorAll('#PlayerCardsDiv');
+const DealerCardDiv = doc.getElementById('DealerCardsDiv');
+
+getCardPaths();
+spawnCard(PlayerCardDiv[0]);
+spawnCard(DealerCardDiv);
+
+
+function spawnCard(SpawnDiv){
+    let img = doc.createElement('img');
+    let rndNum = getRandomCardIndex();
+    img.src = cardPictures[rndNum]; 
+    doc.body.appendChild(img);
+    SpawnDiv.appendChild(img); 
+    img.style.width = '100px';
+    img.style.height = '150px';
+}
+
+function getRandomCardIndex(){
+    return Math.round(Math.random() * 52);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function getCardPaths(){
+for(let i = 0; i < symbols.length; i++){
+    for(let j = 0; j < numbers.length; j++){
+        cardPictures.push("Cards/"+numbers[j] + '_of_' + symbols[i] + '.png')  ;
+        
+    }
+    for(let j = 0; j < HighCards.length ; j++){
+        cardPictures.push("Cards/"+HighCards[j] + '_of_' + symbols[i] + '2.png');
+        
+    }
+}
+}
