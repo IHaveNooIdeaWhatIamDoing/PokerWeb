9b926de8a1f4a06dea83ffc688cfcb3e995fce05
IHaveNooIdeaWhatIamDoing <domenik08sabitzer@gmail.com>
2025-04-15 18:47:38 +0200

    Geld System
    Viel gefixed funktioniert aber noch nicht so ganz

--- main.js
+++ main.js
@@ -15,6 +15,7 @@ const PotTxt = doc.getElementById('Pot');
 const PlayerIncreaseBtn = doc.querySelectorAll('#IncreaseButton');
 const PlayerCallBtn = doc.querySelectorAll('#CallButton');
 const PlayerCheckBtn = doc.querySelectorAll('#CheckButton');
+const PlayerFoldBtn = doc.querySelectorAll('#FoldButton');
 
 let PlayerBetInputTxt = doc.querySelectorAll('#PlayerBet');
 let PlayerBetInput= [];
@@ -29,6 +30,7 @@ const PlCount=3;
 let Pot;
 let PlayerBets = [];
 let WasRaised;
+let FoldedPlayers = []; //Stores the Index of the players who folded
 //Betting Bools
 let increaseAble;
 let callAble;
@@ -46,12 +48,13 @@ async function Main(){
     //Start Procedure
     console.log(PlayerMoneyTxt);
     assignMoney();
-    UpdateMoney();
-    
+    UpdateMoney();  
     MakeToRightDataType();
     createBets();
+    createFoldedPlayers();
     //Game Starts   
     //First Betting Phase
+    
     await(BetManager());
     //first 3 cards are dealt
     handOutDealerCards(3); //3 cards for dealer
@@ -84,8 +87,9 @@ function spawnCard(SpawnDiv){
     }
  }
  function assignMoney(){
-    for(let i = 0; i < PlCount; i++){ 
-        PlayerMoneyTxt[i].value = 300; //Aktualisiere den Einsatz des Spielers
+    for(let i = 0; i < PlCount; i++){
+        if(i === 2) PlayerMoneyTxt[i].value = 100; 
+        else PlayerMoneyTxt[i].value = 300; //Aktualisiere den Einsatz des Spielers
         
     }
  }
@@ -115,13 +119,19 @@ function createBets(){
     }
 }
 
+function createFoldedPlayers(){
+    for(let i = 0; i < PlCount; i++){
+        FoldedPlayers.push(false); 
+    }
+}
+
 async function BetManager(){ 
 
 return new Promise(async resolve => {
     WasRaised = false;   
     for(let i = 0; i < PlCount; i++){  
         PlayerCardDiv[i].style.border = "5px solid red";     //highlight current player 
-        await(AskForBet(i));  
+        if(FoldedPlayers[i] == false) await(AskForBet(i));  //Wenn Spieler nicht gefoldet hat
         PlayerCardDiv[i].style.border = "5px solid white"; //reset border
         UpdateUi();
         UpdatePot();       
@@ -149,31 +159,48 @@ return new Promise(async resolve => {
     
     
     return new Promise(resolve => {
-        addEventListener("input", CheckForMoney(PlayerNumber)); //Updates on input change
+        
         addEventListener("click", (EventTarget) => {
             if(PlayerNumber === -1) return; //Wenn PlayerNumber -1 ist, wird die Funktion beendet
-            
+            UpdateAll(); //Update all PlayerBetInput
+            CheckForMoney(PlayerNumber); //First Update
             
             PlayerBetInput[PlayerNumber] = parseInt(PlayerBetInputTxt[PlayerNumber].value); //Aktualisiere den Einsatz des Spielers
             
             //Erster Fall: Spieler möchte den Einsatz erhöhen
             if(EventTarget.target === PlayerIncreaseBtn[PlayerNumber] && increaseAble){
-                PlayerMoney[PlayerNumber] -= PlayerBetInput[PlayerNumber] - PlayerBets[PlayerNumber]; //Abziehen des Einsatzes vom Geld des Spielers
-                PlayerBets[PlayerNumber] =+ +(PlayerBetInput[PlayerNumber]);
+                PlayerMoney[PlayerNumber] -= PlayerBetInput[PlayerNumber]  ; //Abziehen des Einsatzes vom Geld des Spielers
+                PlayerBets[PlayerNumber] += +(PlayerBetInput[PlayerNumber]); //Spieler erhöht den Einsatz
                 if(PlayerNumber !== 0)WasRaised = true; //Spieler hat erhöht, aber nur wenn er nicht der Erste ist
-                PlayerNumber = -1; //reset PlayerNumber to -1 to avoid multiple clicks
+               PlayerNumber = -1; 
                 resolve();
             }          
+            
             //Zweiter Fall: Spieler möchte mitgehen
             if(EventTarget.target === PlayerCallBtn[PlayerNumber] && callAble){
                 PlayerMoney[PlayerNumber] -= Math.max(...PlayerBets) - PlayerBets[PlayerNumber]; //Abziehen des Einsatzes vom Geld des Spielers
                 PlayerBets[PlayerNumber] = +Math.max(...PlayerBets); //Spieler geht mit der höchsten Wette mit
-                 
+                PlayerNumber = -1; 
                 resolve(PlayerBets);                
             }
             //Dritter Fall: Spieler möchte checken
             if(EventTarget.target === PlayerCheckBtn[PlayerNumber] && checkAble){ //CHecken ist nur möglich, wenn der Spieler die höchste Wette hat
+                PlayerNumber = -1; //reset PlayerNumber to -1 to avoid multiple clicks
+                
                 resolve(PlayerBets);
+            }
+            //4. Fall: Spieler möchte folden
+            if(EventTarget.target === PlayerFoldBtn[PlayerNumber]){
+                FoldedPlayers[PlayerNumber] = true; //Spieler foldet und wird zur Liste der gefoldeten Spieler hinzugefügt
+                PlayerNumber = -1; //reset PlayerNumber to -1 to avoid multiple clicks              
+                resolve(PlayerBets);
+            }
+            //5. Fall: ALL IN!
+            if(EventTarget.target === PlayerCallBtn[PlayerNumber] && !callAble && !increaseAble && !checkAble){
+                PlayerBets[PlayerNumber] += PlayerMoney[PlayerNumber]; //Spieler geht all in
+                PlayerMoney[PlayerNumber] = 0; //Spieler hat kein Geld mehr
+                PlayerNumber = -1; //reset PlayerNumber to -1 to avoid multiple clicks
+                resolve(PlayerBets); 
             }
             
         })
@@ -186,23 +213,40 @@ function CheckForMoney(i){
         //Update input
         if(i === -1) return;
         PlayerBetInput[i] = parseInt(PlayerBetInputTxt[i].value); 
-        console.log("PlayerBetInput: "+ PlayerBetInput[i] + " PlayerMoney: "+ PlayerMoney[i] + "Max Bet "+ Math.max(...PlayerBets)); ;
+        
         //1. Fall: höchste Bette und genug Geld -> increaseAble
-        if(PlayerBetInput[i] >= Math.max(...PlayerBets) && PlayerMoney[i] >= PlayerBetInput[i]){
+        if(PlayerBetInput[i]  > 0 && PlayerMoney[i] >= PlayerBetInput[i] && PlayerBetInput[i] + PlayerBets[i] > Math.max(...PlayerBets)){
             increaseAble = true;
             callAble = true; 
-            
+            console.log("Case 1");
         }
-        else if(PlayerMoney[i] >= Math.max(...PlayerBets)){
-            increaseAble = false;
+        else increaseAble = false;
+        if(PlayerMoney[i] >= Math.max(...PlayerBets)){    
             callAble = true;
+            console.log("Case 2");
         }
         if(PlayerBets[i] == Math.max(...PlayerBets)){
             checkAble = true;
             callAble = false;
+            console.log("Case 3");
         }
         else checkAble = false;
 
+        if(PlayerMoney[i] <= Math.max(...PlayerBets) && PlayerBets[i] != Math.max(...PlayerBets)){
+            callAble = false; 
+            increaseAble = false;
+            checkAble = false;
+            console.log("Case 4");
+        }
+
+        UpdateUi(); 
+}
+
+function UpdateAll(){
+    for(let i = 0; i < PlCount; i++){
+        PlayerBetInput[i] = parseInt(PlayerBetInputTxt[i].value); //Aktualisiere den Einsatz des Spielers
+        console.log("GotUpdated");
+    }
 }
 
  function UpdatePot(){
@@ -217,7 +261,10 @@ function CheckForMoney(i){
     //UpdateMoney
     for(let i = 0; i < PlCount; i++){
         PlayerMoneyTxt[i].value = PlayerMoney[i]; //Aktualisiere den Einsatz des Spielers
-        
+        //Update Player Butttons
+        PlayerIncreaseBtn[i].disabled = !increaseAble;
+        PlayerCallBtn[i].disabled = !callAble;
+        PlayerCheckBtn[i].disabled = !checkAble;
     }
  }
 
@@ -232,4 +279,13 @@ for(let i = 0; i < symbols.length; i++){
         
     }
 }
+}
+
+function Debug(i){
+    console.log("Highest Bet: "+ Math.max(...PlayerBets));
+    console.log("PlayerBetInput: "+ PlayerBetInput[i]);
+    console.log("PlayerBet: "+ PlayerBets[i]);
+    console.log("CheckAble: "+ checkAble);
+    console.log("CallAble: "+ callAble);
+    console.log("IncreaseAble: "+ increaseAble);
 }
